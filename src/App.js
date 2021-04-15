27a4c120070621392d4b89a0f274278124cb4b9c
pedroBotega99 <pedro.botega@ateliedepropaganda.com.br>
2021-04-15 08:19:02 -0300

    input e button criados

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { Button, Row, Col, Input } from 'reactstrap';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Row>
+        <Col>
+          <form>
+            <Input
+              placeholder="Cole o link do vídeo aqui"
+            />
+            <Button color="primary">Enviar</Button>
+          </form>
+        </Col>
+      </Row>
+    </div>  
   );
 }
 
-export default App;
+export default App
